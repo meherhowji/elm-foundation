9c63a11c8739b49e23bfa3ffd60a6f3087685b3d
Meher Ranjan <hmeherranjan@gmail.com>
2018-09-22 03:12:52 +0530

    grid layout styles, flat color scheme

--- src/Foundation/CDN.elm
+++ src/Foundation/CDN.elm
@@ -1,4 +1,4 @@
-module Foundation.CDN exposing (stylesheet)
+module Foundation.CDN exposing (stylesheet, stylesheetTheme, stylesheetFont)
 
 import Html exposing (Html, node)
 import Html.Attributes exposing (rel, href)
@@ -7,6 +7,21 @@ stylesheet : Html msg
 stylesheet =
     node "link"
         [ rel "stylesheet"
-        , href "foundation-xy.css"
+        , href "https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.0-rc.3/css/foundation.css"
+        ]
+        []
+
+stylesheetFont : Html msg
+stylesheetFont = 
+    node "link"
+        [ rel "stylesheet"
+        , href "https://fonts.googleapis.com/css?family=Abril+Fatface|Fredoka+One" ]
+        []
+
+stylesheetTheme : Html msg
+stylesheetTheme =
+    node "link"
+        [ rel "stylesheet"
+        , href "grid-colorise.css"
         ]
         []
